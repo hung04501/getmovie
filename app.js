0b3a95a37efdf9839db4061033d50a62572c5cbf
HungNQ <HungNQ@gmail.com>
2016-06-14 10:07:12 +0700

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 var dbName='movieDB';
 
 //provide a sensible default for local development
-mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + dbName;
+mongodb_connection_string = 'mongodb://getmovie.herokuapp.com:27017/' + dbName;
 //take advantage of openshift env vars when available:
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
   mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + dbName;
